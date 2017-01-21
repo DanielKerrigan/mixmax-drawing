1a3da7dc4f3ad376de571c946c564906417266c0
Daniel Kerrigan <dkerriga@nd.edu>
2017-01-21 01:34:52 -0500

    got something working

--- api/resolver.js
+++ api/resolver.js
@@ -1,13 +1,12 @@
 module.exports = function(req, res) {
-  console.log('req.body', req.body)
+  console.log('req.body', req.body);
   var data = JSON.parse(req.body.params);
   if (!data) {
     res.status(403 /* Unauthorized */ ).send('Invalid params');
     return;
   }
-
-  var width = data.width > 600 ? 600 : data.width;
-  var html = '<img style="max-width:100%;" src="' + data.src + '" width="' + width + '"/>';
+  // set the html that will be displayed
+  var html = "<img src="+data.src+">";
   res.json({
     body: html
     // Add raw:true if you're returning content that you want the user to be able to edit
